8003847fd75a9a88262c936d20386402eebbaadf
Plague Fox <plugfox@gmail.com>
2021-11-15 16:15:31 +0300

    [quickfix] update test

--- test/l_test.dart
+++ test/l_test.dart
@@ -13,20 +13,26 @@ void mainFunctional() {
     expect(l is L, isTrue);
   });
 
-  test('shouldPrint', () {
+  test('shouldPrint', () async {
+    var count = 0;
+    final sub = l.listen((_) => count++);
+
     l.capture(
-      () => l
-        ..s('Shout')
-        ..v('Regular 1')
-        ..e('Error')
-        ..vv('Regular 2')
-        ..w('Warning')
-        ..vvv('Regular 3')
-        ..i('Info')
-        ..vvvv('Regular 4')
-        ..d('Debug')
-        ..vvvvv('Regular 5')
-        ..vvvvvv('Regular 6'),
+      () {
+        l
+          ..s('01. Shout')
+          ..v('02. Regular 1')
+          ..e('03. Error')
+          ..vv('04. Regular 2')
+          ..w('05. Warning')
+          ..vvv('06. Regular 3')
+          ..i('07. Info')
+          ..vvvv('08. Regular 4')
+          ..d('09. Debug')
+          ..vvvvv('10. Regular 5')
+          ..vvvvvv('11. Regular 6');
+        print('12. Handle print');
+      },
       LogOptions(
         outputInRelease: true,
         handlePrint: true,
@@ -40,6 +46,8 @@ void mainFunctional() {
             '| $message',
       ),
     );
-    expect(true, true);
+    await Future<void>.delayed(Duration.zero);
+    sub.cancel();
+    expect(count, 12);
   });
 }
