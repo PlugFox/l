ba4c18abd49c7fcddb75d837e455e20be1a237fe
Plague Fox <plugfox@gmail.com>
2023-05-13 19:23:27 +0400

    Exclude flutter example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,6 @@ library l.example;
 
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:l/l.dart';
 
 void main() => runZonedGuarded<void>(
@@ -18,12 +17,14 @@ void main() => runZonedGuarded<void>(
             ..s('Shout')
             ..v6('Regular 6');
           print('Hello from original print!');
-          runApp(const App());
           l.v('Running');
+          throw Exception('Exception');
         },
         const LogOptions(
           handlePrint: true,
           messageFormatting: _messageFormatting,
+          outputInRelease: false,
+          printColors: true,
         ),
       ),
       l.e,
@@ -34,59 +35,3 @@ Object _messageFormatting(Object message, LogLevel logLevel, DateTime now) =>
 
 String _timeFormat(DateTime time) =>
     '${time.hour}:${time.minute.toString().padLeft(2, '0')}';
-
-@immutable
-class App extends StatelessWidget {
-  const App({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter [L]ogger example',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter [L]ogger example'),
-          ),
-          body: SafeArea(
-            child: Center(
-              child: SingleChildScrollView(
-                child: SizedBox(
-                  width: 200,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      OutlinedButton(
-                        onPressed: () => l.i(' * on click'),
-                        child: const Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text(
-                            'Click me',
-                            textScaleFactor: 2,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 25),
-                      OutlinedButton(
-                        onPressed: () => scheduleMicrotask(
-                          () => throw UnsupportedError('Some UnsupportedError'),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text(
-                            'Throw error',
-                            textScaleFactor: 2,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-}
