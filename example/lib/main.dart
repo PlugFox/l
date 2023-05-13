556a5c476b3c6b40a076c2c968cf17a06c7b5f3d
Plague Fox <plugfox@gmail.com>
2023-05-13 19:13:48 +0400

    Only one example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,20 +1,39 @@
 // ignore_for_file: public_member_api_docs, avoid_print
+library l.example;
 
 import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:l/l.dart';
 
-void main() => runZonedGuarded(
-      () => l.capture(() {
-        l.d('runApp');
-        print('original print');
-        runApp(const App());
-        l.v('Running');
-        return null;
-      }),
+void main() => runZonedGuarded<void>(
+      () => l.capture<void>(
+        () {
+          l
+            ..v('Regular 1')
+            ..e('Error')
+            ..w('Warning')
+            ..i('Info')
+            ..d('Debug')
+            ..s('Shout')
+            ..v6('Regular 6');
+          print('Hello from original print!');
+          runApp(const App());
+          l.v('Running');
+        },
+        const LogOptions(
+          handlePrint: true,
+          messageFormatting: _messageFormatting,
+        ),
+      ),
       l.e,
     );
+
+Object _messageFormatting(Object message, LogLevel logLevel, DateTime now) =>
+    '${_timeFormat(now)} | $message';
+
+String _timeFormat(DateTime time) =>
+    '${time.hour}:${time.minute.toString().padLeft(2, '0')}';
 
 @immutable
 class App extends StatelessWidget {
