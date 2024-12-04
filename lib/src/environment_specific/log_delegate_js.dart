b0f67bfd17e8d81be6724ff87cfd9a3ef8734ff0
Plague Fox <plugfox@gmail.com>
2024-12-04 12:22:03 +0400

    Remove dart:html from package

--- lib/src/environment_specific/log_delegate_js.dart
+++ lib/src/environment_specific/log_delegate_js.dart
@@ -1,4 +1,4 @@
-import 'dart:html' as html show window, Console;
+import 'dart:js_interop';
 
 import 'package:meta/meta.dart';
 
@@ -7,10 +7,40 @@ import 'log_delegate.dart';
 import 'message_formatting_pipeline.dart';
 import 'message_log_formatting_mixin.dart';
 
+/// The window object
+@internal
+extension type Window._(JSObject _) {
+  /// Returns the console object
+  external Console get console;
+}
+
+/// The console object
+@internal
+extension type Console._(JSObject _) {
+  /// Logs a message to the console
+  external void log(Object? arg);
+
+  /// Logs a warning message to the console
+  external void warn(Object? arg);
+
+  /// Logs an error message to the console
+  external void error(Object? arg);
+
+  /// Logs an info message to the console
+  external void info(Object? arg);
+
+  /// Logs a debug message to the console
+  external void debug(Object? arg);
+}
+
+/// The global window object
+@JS()
+@internal
+external Window get window;
+
 /// Environment-specific implementation of [LogDelegate]
 @internal
-LogDelegate createEnvironmentLogDelegate() =>
-    LogDelegate$JS(html.window.console);
+LogDelegate createEnvironmentLogDelegate() => LogDelegate$JS(window.console);
 
 @internal
 final class LogDelegate$JS implements LogDelegate {
@@ -19,7 +49,7 @@ final class LogDelegate$JS implements LogDelegate {
   final MessageFormattingPipeline _formatter = MessageFormattingPipelineWeb();
 
   @protected
-  final html.Console console;
+  final Console console;
 
   @override
   void log(LogMessage event) {
